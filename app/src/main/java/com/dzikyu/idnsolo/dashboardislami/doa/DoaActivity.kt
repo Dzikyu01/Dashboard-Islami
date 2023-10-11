b91a3bbbb1a484c4337b39381c64cadd344a64d3
Dzikyu01 <dzikralisafadli@gmail.com>
2023-10-11 09:17:27 +0700

    DI-Data Update

--- app/src/main/java/com/dzikyu/idnsolo/dashboardislami/doa/DoaActivity.kt
+++ app/src/main/java/com/dzikyu/idnsolo/dashboardislami/doa/DoaActivity.kt
@@ -29,8 +29,46 @@ class DoaActivity : AppCompatActivity() {
             val intent = Intent (this, ListDoaActivity::class.java)
             intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.pagi_dan_malam))
             intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_pagi_malam)
+            startActivity(intent)
+        }
+
+        binding.cardDoaRumah.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.doa_rumah))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_rumah)
+            startActivity(intent)
+        }
+
+        binding.cardMakananMinuman.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.makanan_dan_minuman))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_makanan_minuman)
+            startActivity(intent)
+        }
+
+        binding.cardDoaPerjalanan.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.doa_perjalanan))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_perjalanan)
+            startActivity(intent)
+        }
+
+        binding.cardDoaSholat.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.doa_sholat))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_sholat)
+            startActivity(intent)
+        }
+
+        binding.cardEtikaBaik.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.etika_baik))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_etika_baik)
+            startActivity(intent)
         }
     }
+
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
--- app/src/main/java/com/dzikyu/idnsolo/dashboardislami/doa/ListDoaActivity.kt
+++ app/src/main/java/com/dzikyu/idnsolo/dashboardislami/doa/ListDoaActivity.kt
@@ -2,12 +2,73 @@ package com.dzikyu.idnsolo.dashboardislami.doa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dzikyu.idnsolo.dashboardislami.R
+import com.dzikyu.idnsolo.dashboardislami.databinding.ActivityDetailDoaBinding
+import com.dzikyu.idnsolo.dashboardislami.databinding.ActivityDetailListDoaBinding
+import com.dzikyu.idnsolo.dashboardislami.doa.adapter.DoaListAdapter
+import com.dzikyu.idnsolo.dashboardislami.doa.data.DoaEtikaBaik
+import com.dzikyu.idnsolo.dashboardislami.doa.data.DoaMakananMinuman
+import com.dzikyu.idnsolo.dashboardislami.doa.data.DoaPagiMalam
+import com.dzikyu.idnsolo.dashboardislami.doa.data.DoaPerjalanan
+import com.dzikyu.idnsolo.dashboardislami.doa.data.DoaRumah
+import com.dzikyu.idnsolo.dashboardislami.doa.data.DoaSholat
+import com.dzikyu.idnsolo.dashboardislami.doa.model.DoaModel
+import java.security.KeyStore.TrustedCertificateEntry
 
 class ListDoaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailListDoaBinding
+    lateinit var title:String
+    var logo: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_list_doa)
+        binding = ActivityDetailListDoaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = intent.getStringExtra(EXTRA_TITLE).toString()
+        logo = intent.getIntExtra(EXTRA_IMAGE,0)
+
+        val actionBar = supportActionBar
+        actionBar?.title = intent.getStringExtra(EXTRA_TITLE)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initRecyclerView(
+
+        )
+
+    }
+
+    private fun initRecyclerView() {
+        val list : ArrayList<DoaModel> = arrayListOf()
+        when (title){
+            getString(R.string.pagi_dan_malam)->{
+                list.addAll(DoaPagiMalam.listDoaPagiDanMalamData)
+            }
+            getString(R.string.doa_rumah) ->{
+                list.addAll(DoaRumah.listDoaRumahData)
+            }
+            getString(R.string.makanan_dan_minuman) ->{
+                list.addAll(DoaMakananMinuman.listDoaMakananDanMinumanData)
+            }
+            getString(R.string.perjalanan) ->{
+                list.addAll(DoaPerjalanan.listDoaPerjalananData)
+            }
+            getString(R.string.doa_sholat) ->{
+                list.addAll(DoaSholat.listDoaShalatData)
+            }
+            getString(R.string.etika_baik) ->{
+                list.addAll(DoaEtikaBaik.listDoaEtikaBaikData)
+            }
+        }
+
+        binding.rvListDoa.hasFixedSize()
+        binding.rvListDoa.layoutManager = LinearLayoutManager(this)
+        val adapter = DoaListAdapter(list, title, logo)
+        binding.rvListDoa.adapter = adapter
+
     }
 
     companion object{
