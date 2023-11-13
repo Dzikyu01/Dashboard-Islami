ec622d351dd5abebcb8890936fe190fee4f303cb
risnanto73 <tiolast63@gmail.com>
2023-11-13 08:54:57 +0700

    add detail kajian

--- app/src/main/java/com/dzikyu/idnsolo/dashboardislami/kajian/DetailKajianActivity.kt
+++ app/src/main/java/com/dzikyu/idnsolo/dashboardislami/kajian/DetailKajianActivity.kt
@@ -1,10 +1,15 @@
 package com.dzikyu.idnsolo.dashboardislami.kajian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dzikyu.idnsolo.dashboardislami.R
 import com.dzikyu.idnsolo.dashboardislami.databinding.ActivityDetailDoaBinding
 import com.dzikyu.idnsolo.dashboardislami.databinding.ActivityDetailKajianBinding
+import com.dzikyu.idnsolo.dashboardislami.kajian.model.KajianModel
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class DetailKajianActivity : AppCompatActivity() {
 
@@ -13,6 +18,47 @@ class DetailKajianActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailKajianBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val kajian = intent.getParcelableExtra<KajianModel>(EXTRA_KAJIAN) as KajianModel
+        initKajian(kajian)
+    }
+
+    private fun initKajian(kajian: KajianModel) {
+        binding.tvPenceramah.text = kajian.speaker
+        binding.tvChannelYoutube.text = kajian.channel
+        binding.tvJudulKajian.text = kajian.title
+        binding.tvDeskripsi.text = kajian.summary
+
+        val youtubeURL: YouTubePlayerView = binding.youtubePlayerView
+        lifecycle.addObserver(youtubeURL)
+        youtubeURL.addYouTubePlayerListener(
+            object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    youTubePlayer.loadVideo(kajian.urlVideo, 0f)
+                    super.onReady(youTubePlayer)
+                }
+            }
+        )
+
+        binding.fabShare.setOnClickListener {
+            val sendKajian = Intent()
+            sendKajian.action = Intent.ACTION_SEND
+            sendKajian.putExtra(
+                Intent.EXTRA_TEXT, "Kajian dengan judul ${kajian.title}" +
+                        " disampaikan oleh ${kajian.speaker} dapat dilihat di link berikut ini: " +
+                        "https://www.youtube.com/watch?v=${kajian.urlVideo}"
+            )
+            sendKajian.type = "text/plain"
+            startActivity(sendKajian)
+        }
+    }
+
+    //back
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     companion object {
--- app/src/main/java/com/dzikyu/idnsolo/dashboardislami/kajian/KajianActivity.kt
+++ app/src/main/java/com/dzikyu/idnsolo/dashboardislami/kajian/KajianActivity.kt
@@ -3,8 +3,12 @@ package com.dzikyu.idnsolo.dashboardislami.kajian
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dzikyu.idnsolo.dashboardislami.R
 import com.dzikyu.idnsolo.dashboardislami.databinding.ActivityKajianBinding
+import com.dzikyu.idnsolo.dashboardislami.kajian.adapter.KajianAdapter
+import com.dzikyu.idnsolo.dashboardislami.kajian.data.KajianData
+import com.dzikyu.idnsolo.dashboardislami.kajian.model.KajianModel
 
 class KajianActivity : AppCompatActivity() {
 
@@ -16,6 +20,15 @@ class KajianActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.rvKajian.layoutManager = LinearLayoutManager(this)
+        binding.rvKajian.setHasFixedSize(true)
+        val listKajian = ArrayList<KajianModel>()
+        listKajian.addAll(KajianData.listData)
+        binding.rvKajian.adapter = KajianAdapter(listKajian)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
