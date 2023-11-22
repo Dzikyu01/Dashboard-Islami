8c0b6afedd532221394f741c1b286fb44096d8cd
Dzikyu01 <dzikralisafadli@gmail.com>
2023-11-22 08:46:54 +0700

    DI - Features Sholat

--- app/src/main/java/com/dzikyu/idnsolo/dashboardislami/sholat/SholatActivity.kt
+++ app/src/main/java/com/dzikyu/idnsolo/dashboardislami/sholat/SholatActivity.kt
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.dzikyu.idnsolo.dashboardislami.R
 import com.dzikyu.idnsolo.dashboardislami.databinding.ActivitySholatBinding
 import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -34,6 +39,44 @@ class SholatActivity : AppCompatActivity() {
         val client = AsyncHttpClient()
         val url = "https://api.myquran.com/v1/sholat/jadwal/$location/$tahun/$bulan/$tanggal"
         Log.d("SholatActivity","getPrayTimeData: $url")
+
+        client.get(url,object :AsyncHttpResponseHandler(){
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                binding.pbJadwalSholat.visibility = View.INVISIBLE
+                val responseData = responseBody?.let {String(it)}
+
+                try {
+                    // Parsing json
+                    val responseObject = JSONObject(responseData)
+                    val data = responseObject.getJSONObject("data")
+                    val jadwal = data.getJSONObject("jadwal")
+                    val lokasi = data.getJSONObject("lokasi")
+                    binding.tvLocation.text = lokasi.getString("kota")
+                    binding.tvDatePray.text = jadwal.getString("tanggal")
+                    binding.tvPrayTimeSubuh.text =jadwal.getString("subuh")
+                    binding.tvPrayTimeDzuhur.text = jadwal.getString("dzuhur")
+                    binding.tvPrayTimeAshar.text = jadwal.getString("ashar")
+                    binding.tvPrayTimeMaghrib.text = jadwal.getString("maghrib")
+                    binding.tvPrayTimeIsya.text = jadwal.getString("isya")
+                } catch (error: Exception){
+                    Toast.makeText(this@SholatActivity, error.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                Toast.makeText(this@SholatActivity, error?.message , Toast.LENGTH_SHORT).show()
+            }
+
+        })
 //        tes
     }
 
